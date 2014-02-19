a7c73458438885ad73cdd28473eac03396a85f16
hadoop@ns3-dev <hadoop@ns3-dev.sekiya-lab.info>
2014-02-19 14:06:14 +0900

    add host command line argument

--- tools/tweet_with_api_key.py
+++ tools/tweet_with_api_key.py
@@ -9,14 +9,15 @@ import requests # pip install requests
 import re
 
 if len(sys.argv) < 3:
-    print "Usage: %s UserName API_Key [reply tweet ID]" % sys.argv[0]
+    print "Usage: %s Host UserName API_Key [reply tweet ID]" % sys.argv[0]
     exit(1)
 
-user_name = sys.argv[1]
-api_key = sys.argv[2]
+host = sys.argv[1]
+user_name = sys.argv[2]
+api_key = sys.argv[3]
 reply_to = None
-if len(sys.argv) > 3:
-    reply_to = int(sys.argv[3])
+if len(sys.argv) > 4:
+    reply_to = int(sys.argv[4])
 
 tweet = sys.stdin.read()
 
@@ -27,7 +28,7 @@ request_data['text'] = tweet
 if reply_to is not None:
     request_data['reply_to'] = reply_to
 
-req = requests.post('http://[::1]:8000/post.json',
+req = requests.post('http://' + host + ':8000/post.json',
         headers={'content-type': 'application/json; charset=utf-8'},
         data=json.dumps(request_data))
 if req.status_code != 200:
