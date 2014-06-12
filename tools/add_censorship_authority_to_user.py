d879d21e963a8a60ada76eaa506375bf15bb9b8b
IIMURA Takuji <iimura@nc.u-tokyo.ac.jp>
2014-06-12 15:04:18 +0900

    検閲権限を与えるためのコマンドを追加

---
+++ tools/add_censorship_authority_to_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+# ユーザに公開可能権限を追加します。
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+from NECOMATter import NECOMATter
+
+world = NECOMATter("http://localhost:7474")
+
+if len(sys.argv) != 2:
+    print "Usage: %s UserName" % sys.argv[0]
+    exit(1)
+
+user_name = sys.argv[1]
+
+result = world.AssignCensorshipAuthorityToUserByName(user_name)
+if result[0] != True:
+    print "add user failed. ", result[1]
+    exit(1)
+
+print "user %s was given censorship authority.", user_node
+
+
+
